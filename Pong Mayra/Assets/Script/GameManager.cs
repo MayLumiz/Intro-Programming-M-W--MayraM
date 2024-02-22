f678c65e8b14d1c76f6ab4da62a5a765fed4e66f
MayLumiz <158365620+MayLumiz@users.noreply.github.com>
2024-02-22 18:21:01 -0500

    Ball & Paddle script overhaul

--- Pong Mayra/Assets/Script/GameManager.cs
+++ Pong Mayra/Assets/Script/GameManager.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    public TextMeshProGUI playerScoreText;
+    public TextMeshProGUI playerScoreText;
+
+    private int playerScore = 0
+    private int platerScore = 0 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerScoreText.text = "P1" " 
+        playerScoreText.text = "P2"
     }
 
     // Update is called once per frame
