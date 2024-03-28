2b6366e89181201229a9d677e62868420125846b
MayLumiz <158365620+MayLumiz@users.noreply.github.com>
2024-03-27 20:35:16 -0400

    Last comments

--- Pong_CCNYMayra/Assets/Scripts/Ball.cs
+++ Pong_CCNYMayra/Assets/Scripts/Ball.cs
@@ -5,42 +5,42 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
    
-    public Rigidbody2D rb2d;
-    public float maxInitialAngle = 0.67f;
-    public float moveSpeed = 1f;
-    public float maxStarty = 4f;
-    public float speedMultiplier = 1.1f;
-    private float startX = 0f;
+    public Rigidbody2D rb2d; // control ball physics 
+    public float maxInitialAngle = 0.67f; // mac angle for intial direction
+    public float moveSpeed = 1f; // speed of the ball
+    public float maxStarty = 4f; // max y for start position
+    public float speedMultiplier = 1.1f; // speed increase on paddle hit
+    private float startX = 0f; // start x position
 
     private void Start()
     {
-        InitialPush(); 
-        GameManager.instance.onReset += ResetBall;
+        InitialPush(); // start ball movement 
+        GameManager.instance.onReset += ResetBall; // reset on event 
     }
-    private void ResetBall() 
+    private void ResetBall() // reset ball for new round 
     {
       ResetBallPosition();
-      InitialPush();
+      InitialPush(); // start moving again
     }
-    private void InitialPush()
+    private void InitialPush() // intial movement direction
     {
-        Vector2 dir = Random.value < 0.5f ? Vector2.left : Vector2.right;
+        Vector2 dir = Random.value < 0.5f ? Vector2.left : Vector2.right; // choose direction
         
-        dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);
-        rb2d.velocity = dir * moveSpeed;
+        dir.y = Random.Range(-maxInitialAngle, maxInitialAngle); // random y angle 
+        rb2d.velocity = dir * moveSpeed; // apply velocity 
     }
-    private void ResetBallPosition()
+    private void ResetBallPosition() // set ball to start position
     {
-       float posY = Random.Range(-maxStarty, maxStarty);
+       float posY = Random.Range(-maxStarty, maxStarty); // random start y 
        Vector2 position = new Vector2(startX, posY);
-       transform.position = position; 
+       transform.position = position;  // move ball
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision) // dectect score zone 
     {
-        ScoreZone scoreZone = collision.GetComponent<ScoreZone>();
+        ScoreZone scoreZone = collision.GetComponent<ScoreZone>(); // notify score 
         if(scoreZone)
         {
-            GameManager.instance.OnScoreZoneReached(scoreZone.id);
+            GameManager.instance.OnScoreZoneReached(scoreZone.id); // detect paddle hit 
             
             
         }
@@ -50,7 +50,7 @@ public class Ball : MonoBehaviour
         Paddle paddle = collision.collider.GetComponent<Paddle>();
         if (paddle)
         {
-            rb2d.velocity *=speedMultiplier;
+            rb2d.velocity *=speedMultiplier; // increase speed
         }
     }
 }
