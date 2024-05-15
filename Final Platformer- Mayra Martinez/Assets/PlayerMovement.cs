643acc2535837e4d38a8dd03c920bc40782cdb3f
Mayra Martinez <158365620+MayLumiz@users.noreply.github.com>
2024-05-15 18:00:21 -0400

    scene changes and movement

--- Final Platformer- Mayra Martinez/Assets/PlayerMovement.cs
+++ Final Platformer- Mayra Martinez/Assets/PlayerMovement.cs
@@ -6,7 +6,15 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    [Header("PlayerMovement")]
     public float moveSpeed = 5f;
+
+    [Header("Jumping")]
+    public float jumpPower = 10f;
+    [Header("GroundChek")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
 
     float horizontalMovement;
     // Start is called before the first frame update
@@ -23,5 +31,23 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            //hold down jump for big jump
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+        else if (context.canceled)
+        {
+            //little jump 
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawCube(groundCheckPos.position, groundCheckSize);
     }
 }
