db270ada092ef4da5d3abdbb94ad73e62aa70a91
Mayra Martinez <158365620+MayLumiz@users.noreply.github.com>
2024-05-26 00:48:34 -0400

    jump gravity and walls

--- Final Platformer- Mayra Martinez/Assets/PlayerMovement.cs
+++ Final Platformer- Mayra Martinez/Assets/PlayerMovement.cs
@@ -8,15 +8,20 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     [Header("PlayerMovement")]
     public float moveSpeed = 5f;
-
+    float horizontalMovement;
     [Header("Jumping")]
     public float jumpPower = 10f;
-    [Header("GroundChek")]
+    public int maxJumps = 2;
+    int jumpsRemaining;
+    [Header("GroundCheck")]
     public Transform groundCheckPos;
     public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
     public LayerMask groundLayer;
+    [Header("Gravity")]
+    public float baseGravity = 2;
+    public float maxFallSpeed = 18f;
+    public float fallSpeedMultiplier = 2f;
 
-    float horizontalMovement;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +32,20 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
+        GroundCheck();
+        Gravity();
+    }
+    private void Gravity()
+    {
+        if(rb.velocity.y < 0)
+        {
+            rb.gravityScale = baseGravity * fallSpeedMultiplier; //fall faster 
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed));
+        }
+        else
+        {
+            rb.gravityScale = baseGravity;
+        }
     }
     public void Move(InputAction.CallbackContext context)
     {
@@ -34,15 +53,27 @@ public class PlayerMovement : MonoBehaviour
     }
     public void Jump(InputAction.CallbackContext context)
     {
+        if (jumpsRemaining > 0)
+        {
         if (context.performed)
         {
             //hold down jump for big jump
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            jumpsRemaining--;
         }
         else if (context.canceled)
         {
             //little jump 
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            jumpsRemaining--;
+        }
+        }
+    }
+    private void GroundCheck()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            jumpsRemaining = maxJumps;
         }
     }
     private void OnDrawGizmosSelected()
