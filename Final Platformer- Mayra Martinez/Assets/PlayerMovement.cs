8ef607b84a3a2b97959637a51fac2426aa9d26ef
Mayra Martinez <158365620+MayLumiz@users.noreply.github.com>
2024-05-26 18:01:27 -0400

    Camera bounderies

--- Final Platformer- Mayra Martinez/Assets/PlayerMovement.cs
+++ Final Platformer- Mayra Martinez/Assets/PlayerMovement.cs
@@ -154,7 +154,7 @@ public class PlayerMovement : MonoBehaviour
     }
     private void CancelWallJump()
     {
-        isWallJumping = false;
+        isWallJumping = true;
     }
     private void Flip()
     {
